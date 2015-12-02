5fc2e70df92a2ebc12a3e6eb3d737ee75ed2e569
AndBoTheCat <andbogc@gmail.com>
2015-12-02 12:01:11 +0200

    First one
    Added hid usb devices searching and reading

---
+++ usb_monitor/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace usb_monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(779, 159);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 206);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 413);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ usb_monitor/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using USBHIDDRIVER;
+using System.Threading;
+
+namespace usb_monitor
+{
+    public partial class Form1 : Form
+    {
+        string[] s;
+        string current_us;
+        static string abc ="";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //список всех hid устройств
+
+            USBInterface usb = new USBInterface("_");
+            s = usb.getDeviceList();
+
+            //мини парсер 
+
+            for (int x = 0; x < s.Length; x++)
+            {
+                string tmp = "";
+                for (int i = s[x].IndexOf("vid"), j = 0; i < s[x].IndexOf("vid") + 8; i++, j++)
+                {
+                    tmp += Convert.ToString(s[x][i]);
+                }
+                tmp += " ";
+
+                for (int i = s[x].IndexOf("pid"), j = 0; i < s[x].IndexOf("pid") + 8; i++, j++)
+                {
+                    tmp += Convert.ToString(s[x][i]);
+                }
+
+                s[x] = tmp + " ";
+            }
+
+            comboBox1.Enabled = true;
+            comboBox1.DataSource = s;
+            comboBox1.SelectedIndex = -1;
+        }
+
+        static void event_cacher(object sender, EventArgs a)
+        {
+            byte[] currentRecord = null;
+            currentRecord = (byte[])USBHIDDRIVER.USBInterface.usbBuffer[0];
+            for (int i = 0; i < currentRecord.Length; i++)
+            {
+                abc += currentRecord[i];
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            current_us = (string)comboBox1.SelectedValue;
+            textBox1.Text = "";
+
+            //без понятия, как прочитать то, что отправляет устройство
+            if (comboBox1.SelectedIndex != -1)
+            {
+                USBInterface us = new USBInterface(current_us.Split(' ')[0], current_us.Split(' ')[1]);
+                string[] s;
+                //us.enableUsbBufferEvent(new System.EventHandler(event_cacher));
+                us.Connect();
+                us.startRead();
+                Thread.Sleep(5);
+                byte[] currentRecord = null;
+                for (int i = 0; i < 200; i++)
+                {
+                    if (USBInterface.usbBuffer.Count != 0)
+                    {
+                        currentRecord = (byte[])USBInterface.usbBuffer[0];
+                        for (int j = 0; j < currentRecord.Length; j++)
+                        {
+                            abc += currentRecord[j];
+                        }
+                    }
+                    Thread.Sleep(2);
+                }
+                us.stopRead();
+                textBox1.Text = abc;
+            }
+        }
+    }
+}
