d9b247bef324e14db7a5b3acbd76ee7223cdea8a
AndBoTheCat <andbogc@gmail.com>
2015-12-02 21:41:37 +0200

    Not hid only now!
    Everything has changed! Now using libusb.net
    (http://libusbdotnet.sourceforge.net/V2/Index.html). It can connect and
    read from every device.

--- usb_monitor/Form1.cs
+++ usb_monitor/Form1.cs
@@ -9,14 +9,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using USBHIDDRIVER;
 using System.Threading;
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
 
 namespace usb_monitor
 {
     public partial class Form1 : Form
     {
-        string[] s;
+        string s;
         string current_us;
-        static string abc ="";
+        static string abc = "";
+
+        public static UsbDevice MyUsbDevice;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,76 +30,90 @@ namespace usb_monitor
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            //список всех hid устройств
-
-            USBInterface usb = new USBInterface("_");
-            s = usb.getDeviceList();
-
-            //мини парсер 
-
-            for (int x = 0; x < s.Length; x++)
+            ErrorCode ec = ErrorCode.None;
+            UsbRegDeviceList allDevices = UsbDevice.AllDevices;
+            string vid = "";
+            string pid = "";
+            foreach (UsbRegistry usbRegistry in allDevices)
             {
-                string tmp = "";
-                for (int i = s[x].IndexOf("vid"), j = 0; i < s[x].IndexOf("vid") + 8; i++, j++)
+                int i = 0;
+                if (usbRegistry.Open(out MyUsbDevice))
                 {
-                    tmp += Convert.ToString(s[x][i]);
-                }
-                tmp += " ";
+                    s = MyUsbDevice.Info.ToString();
+                    i = s.IndexOf("Vendor") + 9;
+                    while (s[i] != '\n')
+                    {
+                        vid += s[i];
+                        i++;
+                    }
 
-                for (int i = s[x].IndexOf("pid"), j = 0; i < s[x].IndexOf("pid") + 8; i++, j++)
-                {
-                    tmp += Convert.ToString(s[x][i]);
-                }
+                    i = s.IndexOf("ProductID") + 10;
+                    while (s[i] != '\n')
+                    {
+                        pid += s[i];
+                        i++;
+                    }
 
-                s[x] = tmp + " ";
+                    textBox1.Text = textBox1.Text + "\n" + s + vid + pid;
+                }
             }
+            UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(Convert.ToInt32("0x0810", 16), Convert.ToInt32("0x0003", 16));
+            try
+            {
+                MyUsbDevice = UsbDevice.OpenUsbDevice(MyUsbFinder);
 
-            comboBox1.Enabled = true;
-            comboBox1.DataSource = s;
-            comboBox1.SelectedIndex = -1;
+                if (MyUsbDevice == null) throw new Exception("Device Not Found.");
+
+                IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                if (!ReferenceEquals(wholeUsbDevice, null))
+                {
+                    wholeUsbDevice.SetConfiguration(1);
+                    wholeUsbDevice.ClaimInterface(0);
+                }
+                UsbEndpointReader reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
+
+                byte[] readBuffer = new byte[1024];
+                while (ec == ErrorCode.None)
+                {
+                    int bytesRead;
+
+                    ec = reader.Read(readBuffer, 5000, out bytesRead);
+
+                    if (bytesRead == 0) throw new Exception(string.Format("{0}:No more bytes!", ec));
+                    textBox1.Text = textBox1.Text + "\n" + Encoding.Default.GetString(readBuffer, 0, bytesRead);
+                }
+
+                Console.WriteLine("\r\nDone!\r\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message);
+            }
+            finally
+            {
+                if (MyUsbDevice != null)
+                {
+                    if (MyUsbDevice.IsOpen)
+                    {
+                        IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                        if (!ReferenceEquals(wholeUsbDevice, null))
+                        {
+                            wholeUsbDevice.ReleaseInterface(0);
+                        }
+
+                        MyUsbDevice.Close();
+                    }
+                    MyUsbDevice = null;
+                    UsbDevice.Exit();
+
+                }
+            }
         }
 
-        static void event_cacher(object sender, EventArgs a)
-        {
-            byte[] currentRecord = null;
-            currentRecord = (byte[])USBHIDDRIVER.USBInterface.usbBuffer[0];
-            for (int i = 0; i < currentRecord.Length; i++)
-            {
-                abc += currentRecord[i];
-            }
-        }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            current_us = (string)comboBox1.SelectedValue;
-            textBox1.Text = "";
 
-            //без понятия, как прочитать то, что отправляет устройство
-            if (comboBox1.SelectedIndex != -1)
-            {
-                USBInterface us = new USBInterface(current_us.Split(' ')[0], current_us.Split(' ')[1]);
-                string[] s;
-                //us.enableUsbBufferEvent(new System.EventHandler(event_cacher));
-                us.Connect();
-                us.startRead();
-                Thread.Sleep(5);
-                byte[] currentRecord = null;
-                for (int i = 0; i < 200; i++)
-                {
-                    if (USBInterface.usbBuffer.Count != 0)
-                    {
-                        currentRecord = (byte[])USBInterface.usbBuffer[0];
-                        for (int j = 0; j < currentRecord.Length; j++)
-                        {
-                            abc += currentRecord[j];
-                        }
-                    }
-                    Thread.Sleep(2);
-                }
-                us.stopRead();
-                textBox1.Text = abc;
-            }
         }
     }
 }
